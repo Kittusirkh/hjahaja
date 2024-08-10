c4540319087ca8683629e9803a220346b4bd3dca
Pranav Ajay <86167689+itspranavajay@users.noreply.github.com>
2024-08-10 21:39:23 +0530

    Update lora.py

--- lora.py
+++ lora.py
@@ -2,7 +2,7 @@ import torch
 from safetensors import safe_open
 
 # Path to your SafeTensors file
-file_path = "pytorch_lora_weights.safetensors’"
+file_path = "pytorch_lora_weights.safetensors"
 
 # Open the SafeTensors file using PyTorch
 with safe_open(file_path, framework="torch") as f:
